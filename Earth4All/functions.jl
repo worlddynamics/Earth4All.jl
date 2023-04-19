e2aea5ef42d2c342127d8b79e78e005fbd39ce9a
aurorarossi <aurora98rossi@gmail.com>
2023-04-19 15:11:49 +0200

    Add `smoothi` function

--- Earth4All/functions.jl
+++ Earth4All/functions.jl
@@ -15,3 +15,11 @@ end
 Returns `0` until the `startx` and then slopes upward until `endx` and then holds constant. This function corresponds to the `RAMP` function in the `VENSIM` language.
 """
 ramp(x, slope, startx, endx) = IfElse.ifelse(x > startx, IfElse.ifelse(x < endx, slope * (x - startx), slope * (endx - startx)), 0)
+
+"""
+   smoothi!(eqs, x, input, delay_time)
+   Returns a exponential smooth of the input.
+"""
+function smoothi!(eqs, x, input, delay_time)
+   append!(eqs, [D(x) ~ (input - x) / delay_time])
+end
