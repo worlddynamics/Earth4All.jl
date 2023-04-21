ee198a4d7aba6d629cfa8cebbbe95111b288f0bc
Pierluigi Crescenzi <pierluigi.crescenzi@gssi.it>
2023-04-21 14:47:20 +0200

    Create earth4all folder for global scenarios

--- Earth4All/earth4all/scenarios.jl
+++ Earth4All/earth4all/scenarios.jl
@@ -17,35 +17,36 @@ function e4a_run(; kwargs...)
     #     pop, pop_sup, wb, wb_sup,
     # ]
 
-    connection_eqs = [
-        fin.OGR ~ fin_sup.OGR
-        fin.IR ~ fin_sup.IR
-        fin.UR ~ fin_sup.UR
-        pop.GDP ~ pop_sup.GDP
-        pop.IPP ~ wb.IPP
-        pop.OW ~ pop_sup.OW
-        pub.CTA ~ pub_sup.CTA
-        pub.GDP ~ pub_sup.GDP
-        pub.CTPIS ~ pub_sup.CTPIS
-        pub.IPT ~ pub_sup.IPT
-        pub.OW2022 ~ pub_sup.OW2022
-        pub.OW ~ pub_sup.OW
-        pub.GP ~ pub_sup.GP
-        pub.CPUS ~ pub_sup.CPUS
-        pub.GDP ~ pub_sup.GDP
-        pub.POP ~ pop.POP
-        pub.GDPP ~ pop.GDPP
-        pub.II ~ pub_sup.II
-        pub_GDP.GS ~ pub_GDP_sup.GS
-        pub_GDP.GDP ~ pub_sup.GDP
-        pub_GDP.POP ~ pop.POP
-        wb.GDPP ~ pop.GDPP
-        wb.INEQ ~ wb_sup.INEQ
-        wb.LPR ~ wb_sup.LPR
-        wb.PSP ~ pub_GDP.PSP
-        wb.PW ~ wb_sup.PW
-        wb.WDI ~ wb_sup.WDI
-    ]
+    # connection_eqs = [
+    #     fin.OGR ~ fin_sup.OGR
+    #     fin.IR ~ fin_sup.IR
+    #     fin.UR ~ fin_sup.UR
+    #     pop.GDP ~ pop_sup.GDP
+    #     pop.IPP ~ wb.IPP
+    #     pop.OW ~ pop_sup.OW
+    #     pub.CTA ~ pub_sup.CTA
+    #     pub.GDP ~ pub_sup.GDP
+    #     pub.CTPIS ~ pub_sup.CTPIS
+    #     pub.IPT ~ pub_sup.IPT
+    #     pub.OW2022 ~ pub_sup.OW2022
+    #     pub.OW ~ pub_sup.OW
+    #     pub.GP ~ pub_sup.GP
+    #     pub.CPUS ~ pub_sup.CPUS
+    #     pub.GDP ~ pub_sup.GDP
+    #     pub.POP ~ pop.POP
+    #     pub.GDPP ~ pop.GDPP
+    #     pub.II ~ pub_sup.II
+    #     pub_GDP.GS ~ pub_GDP_sup.GS
+    #     pub_GDP.GDP ~ pub_sup.GDP
+    #     pub_GDP.POP ~ pop.POP
+    #     wb.GDPP ~ pop.GDPP
+    #     wb.INEQ ~ wb_sup.INEQ
+    #     wb.LPR ~ wb_sup.LPR
+    #     wb.PSP ~ pub_GDP.PSP
+    #     wb.PW ~ wb_sup.PW
+    #     wb.WDI ~ wb_sup.WDI
+    # ]
+
     # connection_eqs = [
     #     pop.GDP ~ pop_sup.GDP
     #     pop.IPP ~ wb.IPP
@@ -57,6 +58,9 @@ function e4a_run(; kwargs...)
     #     wb.PW ~ wb_sup.PW
     #     wb.WDI ~ wb_sup.WDI
     # ]
+
+    connection_eqs = variable_connections(systems)
+    println.(connection_eqs)
 
     return WorldDynamics.compose(systems, connection_eqs)
 end
