1c026ff7d3168a4c0f2a8a188b96b90c7e34ee46
aurorarossi <aurora98rossi@gmail.com>
2023-04-28 14:30:09 +0200

    Finish external variables

--- Earth4All/demand/subsystems.jl
+++ Earth4All/demand/subsystems.jl
@@ -9,6 +9,14 @@ function demand(; name, params=_params, inits=_inits, tables=_tables, ranges=_ra
     @variables GDPP(t)
     @variables POP(t)
     @variables IPP(t)
+    @variables WSO(t)
+    @variables NI(t)
+    @variables ECTAF2022(t)
+    @variables GBC(t)
+    @variables WBC(t)
+    @variables WF(t)
+    @variables EGDPP(t)
+
 
     @parameters GITRO = params[:GITRO] [description = "Goal for income tax rate owners"]
     @parameters ITRO2022 = params[:ITRO2022] [description = "Income tax rate owners in 2022"]
@@ -28,6 +36,13 @@ function demand_support(; name, params=_params, inits=_inits, tables=_tables, ra
     @variables GDPP(t) [description = "GDP per person kDollar/p/y"] 
     @variables POP(t) [description = "Population Mp"]
     @variables IPP(t) [description = "Introduction period for policy y"]
+    @variables WSO(t) [description = "Worker share output "]
+    @variables NI(t) [description = "National income GDollar/y"]
+    @variables ECTAF2022(t) [description = "Extra Cost of TAs from 2022"]
+    @variables GBC(t) [description = "Government borrowing cost 1/y"]
+    @variables WBC(t) [description = "Worker borrowing cost 1/y"]
+    @variables WF(t) [description = "Work force Mp"]
+    @variables EGDPP(t) [description = "Effective GDP per person kDollar/p/y"]
 
     
     eqs = []
@@ -35,6 +50,13 @@ function demand_support(; name, params=_params, inits=_inits, tables=_tables, ra
     add_equation!(eqs, GDPP ~ WorldDynamics.interpolate(t, tables[:GDPP], ranges[:GDPP]))
     add_equation!(eqs, POP ~ WorldDynamics.interpolate(t, tables[:POP], ranges[:POP]))
     add_equation!(eqs, IPP ~ WorldDynamics.interpolate(t, tables[:IPP], ranges[:IPP]))
+    add_equation!(eqs, WSO ~ WorldDynamics.interpolate(t, tables[:WSO], ranges[:WSO]))
+    add_equation!(eqs, NI ~ WorldDynamics.interpolate(t, tables[:NI], ranges[:NI]))
+    add_equation!(eqs, ECTAF2022 ~ WorldDynamics.interpolate(t, tables[:ECTAF2022], ranges[:ECTAF2022]))
+    add_equation!(eqs, GBC ~ WorldDynamics.interpolate(t, tables[:GBC], ranges[:GBC]))
+    add_equation!(eqs, WBC ~ WorldDynamics.interpolate(t, tables[:WBC], ranges[:WBC]))
+    add_equation!(eqs, WF ~ WorldDynamics.interpolate(t, tables[:WF], ranges[:WF]))
+    add_equation!(eqs, EGDPP ~ WorldDynamics.interpolate(t, tables[:EGDPP], ranges[:EGDPP]))
     
     return ODESystem(eqs; name=name)
 end
