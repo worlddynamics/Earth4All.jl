823a72760d33c868a92c14887aa935e073e862ed
Pierluigi Crescenzi <pierluigi.crescenzi@gssi.it>
2023-05-05 12:48:00 +0200

    Finish equations Output sector

--- Earth4All/output/initialisations.jl
+++ Earth4All/output/initialisations.jl
@@ -1,5 +1,7 @@
 _inits = Dict{Symbol,Float64}(
+    :ETFP => 1,
     :FACNC => 1.05149, # Taken from Vensim table
+    :LAUS => 3060, # Taken from Labour and market sector
     :WSO => 0.5, # Taken from Labour and market sector
 )
 
