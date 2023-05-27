3f4b219c61540331afa9c3069ac1cef7105219bb
Emanuele Natale <emanuele.natale@inria.fr>
2023-05-27 06:49:32 +0200

    Fix main function renaming

--- Earth4All/earth4all/solutions.jl
+++ Earth4All/earth4all/solutions.jl
@@ -2,9 +2,9 @@ using ModelingToolkit
 using DifferentialEquations
 
 function run_tltl_solution()
-    return WorldDynamics.solve(e4a_run_tltl(), (1980, 2100), solver=Euler(), dt=0.015625, dtmax=0.015625)
+    return WorldDynamics.solve(run_tltl(), (1980, 2100), solver=Euler(), dt=0.015625, dtmax=0.015625)
 end
 
 function run_gl_solution()
-    return WorldDynamics.solve(e4a_run_gl(), (1980, 2100), solver=Euler(), dt=0.015625, dtmax=0.015625)
+    return WorldDynamics.solve(run_gl(), (1980, 2100), solver=Euler(), dt=0.015625, dtmax=0.015625)
 end
