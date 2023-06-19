2206de01c593325850c9821f87aeaf7f9340b024
Emanuele Natale <emanuele.natale@inria.fr>
2023-06-19 17:59:33 +0200

    Better than nothing

---
+++ src/ensemble_example.jl
@@ -0,0 +1,30 @@
+include("Earth4All.jl")
+##
+using ModelingToolkit
+using DifferentialEquations
+using Plots 
+##
+tltl = Earth4All.run_tltl()
+##
+sys = structural_simplify(tltl)
+prob = ODEProblem(sys, [], (1980, 2100))
+##
+# This is the function that will be called to generate a new initial condition. 
+# In this case, we are adding a small amount of normally distributed noise to the initial condition.
+function prob_func(prob)
+    remake(prob, u0 = prob.u0 .* (1 .+ 0.1*randn(length(prob.u0))))
+end
+##
+ensemble_prob = EnsembleProblem(prob, prob_func = prob_func)
+##
+sol = solve(ensemble_prob, Euler(), EnsembleThreads(), trajectories=10; dt=0.015625, dtmax=0.015625)
+##
+summ = EnsembleSummary(sol)
+##
+# you can get the trajectory index with 
+# ```
+# v = sol[1]
+# println.(enumerate(v.prob.f.syms))
+# ```
+plot(summ, fillalpha=.5, trajectories = 30)
+##
