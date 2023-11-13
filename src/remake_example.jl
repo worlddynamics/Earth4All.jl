4fcb459e5a08d01c48345b9994af86f5e4ca9438
Emanuele Natale <emanuele.natale@inria.fr>
2023-11-13 16:48:02 +0100

    function to substitute defaults

--- src/remake_example.jl
+++ src/remake_example.jl
@@ -10,20 +10,30 @@ sys = structural_simplify(tltl)
 prob = ODEProblem(sys, [], (1980, 2100))
 model = prob.f.sys # variables are fields of this
 ##
-def_dic = ModelingToolkit.defaults(sys)
-##
 
 # this is a workaround since I don't understand how to access the values of the dictionary directly; the keys are of type SymbolicUtils.BasicSymbolic...
-names_list = collect(keys(def_dic))
-names_str = string.(names_list)
-nameindices = findall(x -> occursin("CO2A", x), names_str)
+function get_def_id(s::String, names_list)
+    names_str = string.(names_list)
+    nameindices = findall(x -> occursin(s*"(", x), names_str)
+    if length(nameindices) == 0
+        error("No default value found for $s")
+    elseif length(nameindices) > 1
+        error("More than one default value found for $s")
+    else
+        return nameindices[1]
+    end
+end
 
-changed_id = 78
-def_dic[names_list[changed_id]] = 4600.0
+function change_inits(s::String, val::Number, sys)
+    def_dic = ModelingToolkit.defaults(sys)
+    names_list = collect(keys(def_dic))
+    def_dic[get_def_id(s, names_list)] = val
+    return ModelingToolkit.varmap_to_vars(def_dic, states(sys))
+end
+
 ##
-
 # now we can re-build the vector of default values and regenerate the problem
-new_u0 = ModelingToolkit.varmap_to_vars(def_dic, states(sys))
+new_u0 = change_inits("CO2A", 4000., sys)
 prob = ODEProblem(sys, new_u0, (1980, 2100))
 sol = solve(prob)
 ##
